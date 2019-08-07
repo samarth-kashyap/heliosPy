5312bd1331bafc43ef057a68eb5201df33c05035
samarthgk@daahpc <samarthgk@gmail.com>
2019-08-07 13:49:48 +0530

    datafuncs -- loadHMIdata_concat, loadHMIdata_avg

--- datafuncs.py
+++ datafuncs.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import numpy as np
+import sys
 from math import sqrt, pi
 from numpy.polynomial.legendre import legval
 from astropy.io import fits
@@ -104,7 +105,7 @@ def loadnorms(l, n, year):
 			return None;
 		return norms[ind, 0]
 
-def loadHMIdata(l, day=6328, nyears=1):
+def loadHMIdata_concat(l, day=6328, nyears=1):
 	'''
 	Loads time series for a given number of years, 
 	by concatenating the required number of 72-day datasets.
@@ -116,6 +117,12 @@ def loadHMIdata(l, day=6328, nyears=1):
 	Outputs:
 		phi_{l} - (np.ndarray, ndim = 2)[m, omega], frequency series for different m
 	'''
+	try:
+		assert nyears>0
+	except AssertionError:
+		print("AssertionError: num has to be a positive number")
+		sys.exit()
+	
 	daynum = int(nyears*365/72)
 	if daynum<1:
 		daynum=1
@@ -129,3 +136,32 @@ def loadHMIdata(l, day=6328, nyears=1):
 			tempnew = np.concatenate((tempnew, temp), axis=1)
 	return np.fft.ifft(tempnew[:, 0::2] - 1j*tempnew[:, 1::2], axis=1)
 
+def loadHMIdata_avg(l, day=6328, num=1):
+	'''
+	Loads 72-day time series and averages over a given number.
+	Returns the IFFT of the averaged time series.
+	Inputs:
+		l			- (int)	spherical harmonic degree
+		day		- (int) starting day of time series - default = 6328
+		num 	- (int) total number of years - default = 1
+	Outputs:
+		phi_{l} - (np.ndarray, ndim = 2)[m, omega], frequency series for different m
+	'''
+	try:
+		assert num>0
+	except AssertionError:
+		print("AssertionError: While calling loadHMIdata_avg(l, day, num) --> num has to be a positive number")
+		sys.exit()
+	
+	for i in xrange(num):
+		daynew = day + i*72;
+		print("Reading "+'/scratch/jishnu/data/HMI/data/HMI_'+str(l).zfill(3)+'_'+str(daynew).zfill(4)+'.fits')
+		tempopen = fits.open('/scratch/jishnu/data/HMI/data/HMI_'+str(l).zfill(3)+'_'+str(daynew).zfill(4)+'.fits')
+		temp = tempopen[1].data
+		tempopen.close()
+		if i==0:
+			tempnew = temp
+		else:
+			tempnew += temp
+	tempnew /= num
+	return np.fft.ifft(tempnew[:, 0::2] - 1j*tempnew[:, 1::2], axis=1)
